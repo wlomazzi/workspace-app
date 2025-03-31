3a21d8bde68575689ff3bbbdf5f712bbdab4441c
wlomazzi <wlomazzi@gmail.com>
2025-03-31 05:36:00 -0600

    Commit - ajuste vercel

--- public/api/index.js
+++ public/api/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require('path');
 
 // Serve os arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Função serverless
+module.exports = app;
+*/
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve os arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para carregar o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Função serverless
 module.exports = app;
